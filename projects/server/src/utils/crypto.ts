10a9dab7a9dbd815c0267589a1857b9dc7e94ed3
Finley Ge <m13203533462@163.com>
2024-05-19 16:35:27 +0800

    chore: delete nest server codes

--- projects/server/src/utils/crypto.ts
+++
@@ -1,8 +0,0 @@
-import { createHash } from 'crypto';
-export function generateHash(password: string): string {
-  return createHash('sha256').update(password).digest('hex');
-}
-
-export function compareHash(password: string, hash: string): boolean {
-  return createHash('sha256').update(password).digest('hex') === hash;
-}
