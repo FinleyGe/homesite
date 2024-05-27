17e48a552afd17e420ebadaf75a1fc8edf968c2a
Finley Ge <m13203533462@163.com>
2024-05-27 12:12:10 +0800

    feat: feed button

--- projects/app/pages/blog/index.vue
+++ projects/app/pages/blog/index.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
+import { Rss } from '@vicons/carbon';
 import BlogList from 'blog';
+import Button from '~/components/common/Button.vue';
 const { t, locale } = useI18n();
 const localePath = useLocalePath();
 useHead({
   title: t('blog.list'),
 });
-
+const openFeed = () => {
+  window.open('/blog/rss.xml', '_blank');
+};
 </script>
 <template>
   <div class="max-w-5xl mx-auto">
     <h1 class="font-bold text-2xl">
       {{ t('blog.list') }}
+      <Button @click="openFeed">
+        <template #icon>
+          <Rss />
+        </template>
+      </Button>
     </h1>
     <div class="flex flex-col">
-      <div 
-        v-for="blog in BlogList.sort((a, b) => new Date(b.date) - new Date(a.date))" :key="blog.link"
+      <div
+v-for="blog in BlogList.sort((a, b) => new Date(b.date) - new Date(a.date))" :key="blog.link"
         class="mt-4 bg-pink-200 dark:bg-pink-800 p-4 rounded-lg shadow-md">
         <NuxtLink :to="localePath(`/blog/${blog.link}`)">
           <span class="font-bold text-lg">
