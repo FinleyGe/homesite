7ee149b8fdb946e34484d8a79d45909fa901b4b7
Finley Ge <m13203533462@163.com>
2024-08-25 19:51:51 +0800

    chore: restructure blog

---
+++ projects/blog/blogs/index.ts
@@ -0,0 +1,23 @@
+import blog1 from './blog1'
+import aboutTheHomesite from './about-the-homesite'
+import dingtalkInterview1 from './dingtalk-interview-1'
+import setupAudioDriverOnPixelbook from './setup-audio-driver-on-pixelbook';
+import fromObsidianToNotion from './from-obsidian-to-notion';
+import giteaCiCd from './gitea-ci-cd';
+import treeSitterQuery from './tree-sitter-query';
+import fastgptArticleAnalysis from './fastgpt-english-article-analyse';
+import n1Armbian from './n1-armbian-docker-openwrt-bypass-route';
+import dockerNetworkModel from './docker-network-model';
+
+export const blogs = [
+  blog1,
+  aboutTheHomesite,
+  dingtalkInterview1,
+  setupAudioDriverOnPixelbook,
+  fromObsidianToNotion,
+  giteaCiCd,
+  treeSitterQuery,
+  fastgptArticleAnalysis,
+  n1Armbian,
+  dockerNetworkModel,
+];
