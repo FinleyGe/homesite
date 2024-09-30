63705fd3d7f2764fc7986c35ed423e2d5bbbb35b
Finley Ge <m13203533462@163.com>
2024-10-01 00:05:54 +0800

    feat(new blog): add mdx-hot-reload-preview-tool

--- projects/blog/preview/index.ts
+++ projects/blog/preview/index.ts
@@ -1,56 +1,53 @@
-import { Command } from 'commander';
-import { parse } from '../utils'
-import { readFileSync, writeFileSync, watchFile } from 'fs';
-import { run } from '@mdx-js/mdx';
-import * as jsx from 'vue/jsx-runtime'
-import { renderToString } from 'vue/server-renderer'
-import {
-  start,
-} from 'live-server'
-import { useMDXComponents } from '@mdx-js/vue';
+import { Command } from "commander";
+import { parse } from "../utils";
+import { readFileSync, watchFile, writeFileSync } from "fs";
+import { run } from "@mdx-js/mdx";
+import * as jsx from "vue/jsx-runtime";
+import { renderToString } from "vue/server-renderer";
+import { start } from "live-server";
 
 const program = new Command();
 
 async function generate({ path }: { path: string }) {
-  console.log('generate preview...')
+  console.log("generate preview...");
   try {
-
-    const content = readFileSync(path, 'utf-8')
+    const content = readFileSync(path, "utf-8");
     if (!content) {
-      console.log('no content')
-      return
+      console.log("no content");
+      return;
     }
-    const blog = await parse(content)
+    const blog = await parse(content);
     const code = await run(blog, {
       ...jsx,
-      useMDXComponents,
-    })
-    const html = await renderToString((code.default as any)())
-    const head = `<!DOCTYPE html><head><meta charset="UTF-8"></head>`
-    writeFileSync('preview.html', `${head}<body>${html}</body>`)
+    });
+    const html = await renderToString((code.default as any)());
+    const head = `<head><meta charset="UTF-8"></head>`;
+    writeFileSync("preview.html", `${head}<body>${html}</body>`);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
 async function main() {
-  program.name('blog-preview')
-    .description('preview blog')
-    .requiredOption('-p --path <path>', 'path of blog')
+  program
+    .name("blog-preview")
+    .description("preview blog")
+    .requiredOption("-p --path <path>", "path of blog");
 
   program.parse();
   const options = program.opts();
-  await generate({ path: options.path })
+  await generate({ path: options.path });
   watchFile(options.path, async () => {
-    await generate({ path: options.path })
-  })
+    await generate({ path: options.path });
+  });
 }
 
-console.log('preview, waiting for blog change...')
-main()
+console.log("preview, waiting for blog change...");
+main();
 
-start({
-  file: 'preview.html',
-  port: 8080,
-  open: true,
-})
+// start({
+//   root: "preview.html",
+//   wait: 1000,
+//   port: 8080,
+//   open: true,
+// });
